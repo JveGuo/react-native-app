95b161ad5002fb42dcbf8c4f8c7765d2266b7015
jiawei.guo <jiawei.guo@car2data.cn>
2022-10-20 17:56:55 +0800

    chore: 项目配置

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./src'], // 表示哪个目录开始设置绝对路径
+                alias: {
+                    // 别名的配置
+                    '@/pages': './src/pages',
+                    '@/navigator': './src/navigator',
+                    '@/utils': './src/utils',
+                    '@/components': './src/components',
+                    '@/assets': './src/assets',
+                },
+            },
+        ],
+    ],
 };
