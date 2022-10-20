21f0626d869e28a788eb66372be686f16687759b
jiawei.guo <jiawei.guo@car2data.cn>
2022-10-20 13:38:24 +0800

    init: init

---
+++ README.md
@@ -0,0 +1,25 @@
+
+#  React Native APP
+
+
+Install dependencies
+
+```bash
+yarn install
+```
+
+android
+
+```bash
+yarn android
+# 或者
+yarn react-native run-android
+```
+
+ios
+
+```bash
+yarn ios
+# 或者
+yarn react-native run-ios
+```
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * @Author: jiawei.guo
+ * @Date: 2022-10-20 11:20:06
+ * @Description: 首页
+ */
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>首页</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
